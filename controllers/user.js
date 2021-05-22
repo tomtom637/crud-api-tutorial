fbf7de1ba5ea4ef8041b0378180298eee79269e9
tomtom637 <tomtom637@gmail.com>
2021-05-22 19:55:01 +0200

    added the token secret to the .env file

--- controllers/user.js
+++ controllers/user.js
@@ -29,7 +29,7 @@ exports.login = async (req, res, next) => {
     }
     const token = jwt.sign(
       { userId: user._id },
-      'RANDOM_TOKEN_SECRET',
+      process.env.RANDOM_TOKEN_SECRET,
       { expiresIn: '24h' }
     );
     res.status(200).json({ userId: user._id, token });
